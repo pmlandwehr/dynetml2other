511a3206f80eadff25fb898a33d2dd566258d888
Peter Landwehr <pmlandwehr@gmail.com>
2014-05-31 09:59:04 -0400

    Reorganized files as a Python Package; still need to fill out many empty entries.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'plandweh'
+
+
+from distutils.core import setup
+
+setup(name='dynetml2other',
+      version='0.1',
+      description='DyNetML parsing and manipulation',
+      author='Peter Landwehr',
+      author_email='plandweh@cs.cmu.edu',
+      url='http://pmlandwehr.github.com/dynetml2other',
+      packages=['dynetml2other', 'dynetml2other.dynetmlparsingutils'],
+     )
