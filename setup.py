64ac77053363ecbed8a957ba80c8aa097cfdc01b
Peter Landwehr <pmlandwehr@gmail.com>
2014-06-12 17:43:50 -0500

    Modified setup to only build one package, modified .gitignore to disregard sphinx stuff.

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ from distutils.core import setup
 setup(name='dynetml2other',
       version='0.1',
       description='DyNetML parsing and manipulation',
+      long_description=open('README.md').read(),
       author='Peter Landwehr',
       author_email='plandweh@cs.cmu.edu',
       url='http://pmlandwehr.github.com/dynetml2other',
-      packages=['dynetml2other', 'dynetml2other.dynetmlparsingutils'],
+      requires=['lxml'],
+      packages=['dynetml2other']
      )
